d25d682f68f4444b29205072771267e246c92c6f
Bilal <bs1324@ic.ac.uk>
2025-02-11 14:28:16 +0000

    deployment files

--- src/startup.sh
+++ src/startup.sh
@@ -1,5 +1,2 @@
-# Start the FastAPI server in the background
-uvicorn app:app --host 0.0.0.0 --port 8000 &
-
-# Start the Streamlit app
-streamlit run ui.py --server.port 8501 --server.address 0.0.0.0
+#!/bin/bash
+uvicorn app:app --host 0.0.0.0 --port $PORT
